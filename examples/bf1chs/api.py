c3fa9eccdfe30c6a6f9ed01da8ab33813a77fe24
zyf722 <1124982772@qq.com>
2024-01-18 22:10:46 +0800

    feat: add github api support for auto-update

--- examples/bf1chs/api.py
+++ examples/bf1chs/api.py
@@ -1,4 +1,5 @@
 import os
+from abc import ABC
 from datetime import datetime
 from functools import wraps
 from io import BytesIO
@@ -7,6 +8,7 @@ from typing import Optional, Union
 from zipfile import ZipFile
 
 from requests import Response, Session
+from requests.exceptions import ProxyError, RequestException  # noqa: F401
 
 
 # Decorator to wrap API.
@@ -22,7 +24,7 @@ def api_call(endpoint: str, method: str, params: Optional[dict] = None):
 
     def decorator(func):
         @wraps(func)
-        def wrapper(self: "ParaTranzAPI", *args, **kwargs):
+        def wrapper(self: "BaseAPI", *args, **kwargs):
             url = self._set_url(endpoint)
             response = self.session.request(method, url, params)
             if response.status_code == 200:
@@ -37,13 +39,71 @@ def api_call(endpoint: str, method: str, params: Optional[dict] = None):
     return decorator
 
 
-class ParaTranzAPI:
+class BaseAPI(ABC):
+    """
+    Base class for API.
+    """
+
+    def __init__(self) -> None:
+        self.session = Session()
+        self.base_url: str = ""
+
+    def _set_url(self, endpoint: str) -> str:
+        """
+        Set the url for the request.
+
+        Args:
+            endpoint (str): The endpoint of the request. Should be in form of `/endpoint`.
+        """
+        return f"{self.base_url}{endpoint}"
+
+
+class GithubAPI(BaseAPI):
+    """
+    Wrapper class to interact with Github API.
+    """
+
+    def __init__(self, owner: str, repo: str) -> None:
+        super().__init__()
+        self.owner = owner
+        self.repo = repo
+        self.base_url = f"https://api.github.com/repos/{self.owner}/{self.repo}"
+
+    @api_call(endpoint="/releases", method="GET")
+    def get_latest_asset(
+        self, response: Optional[Response] = None, asset_name: str = ""
+    ):
+        """
+        Get the latest release asset from the repo.
+        """
+        if response is None:
+            raise ValueError("Response is None.")
+
+        latest_asset_url = ""
+        latest_published_time = datetime.min
+        latest_version = ""
+        for release in response.json():
+            if release["assets"]:
+                for asset in release["assets"]:
+                    if asset_name in asset["name"]:
+                        published_time = datetime.strptime(
+                            release["published_at"], "%Y-%m-%dT%H:%M:%SZ"
+                        )
+                        if published_time > latest_published_time:
+                            latest_published_time = published_time
+                            latest_asset_url = asset["browser_download_url"]
+                            latest_version = release["name"].split(" ")[-1]
+
+        return (latest_asset_url, latest_version, latest_published_time)
+
+
+class ParaTranzAPI(BaseAPI):
     """
     Wrapper class to interact with ParaTranz OpenAPI.
     """
 
     def __init__(self, api_token: str, project_id: int) -> None:
-        self.session = Session()
+        super().__init__()
         self.session.headers.update({"Authorization": f"{api_token}"})
         self.project_id = project_id
         self.base_url = f"https://paratranz.cn/api/projects/{self.project_id}"
@@ -64,15 +124,6 @@ class ParaTranzAPI:
         session.headers.update({"Authorization": f"{api_token}"})
         response = session.get(f"https://paratranz.cn/api/projects/{project_id}/files")
         return response.status_code == 200
-
-    def _set_url(self, endpoint: str) -> str:
-        """
-        Set the url for the request.
-
-        Args:
-            endpoint (str): The endpoint of the request. Should be in form of `/endpoint`.
-        """
-        return f"{self.base_url}{endpoint}"
 
     @api_call(endpoint="/artifacts/download", method="GET")
     def download_artifact(
@@ -118,4 +169,5 @@ class ParaTranzAPI:
         for term in response.json()["results"]:
             terms[term["term"]] = term["translation"]
 
+        return terms
         return terms
